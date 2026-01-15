5790ff9adfa89bfb0dc8add9641cf372c00d4105
Kyle Hsiao <kylehsiao@google.com>
2026-01-14 22:10:56 -0800

    NFC Security Vulnerability in Multi-User Mode
    This change converts the setting to be per-user, so each user's preference is stored independently. This prevents one user from changing another user's security settings.
    
    Bug: 368376056
    Bug: 475315815
    Bug: 475447365
    Test: Manually verified that the setting is independent for primary and secondary users.
    (cherry picked from commit f34a889829d3f124407b7e908d72eaf0bfae0dcc)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:fbbe7e8fe0c5dd273a0114eb1b1bbdf113b70170
    Merged-In: I791d49af88c9e439b0ca600b1e64da309097e693
    Change-Id: I791d49af88c9e439b0ca600b1e64da309097e693

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -1090,6 +1090,35 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
     }
 
 
+
+    /**
+     * Returns the SharedPreferences key for the Secure NFC setting for a specific user.
+     * @param userId The user ID.
+     * @return The user-specific preference key.
+     */
+    private String getSecureNfcPreferenceKeyForUser(int userId) {
+        return PREF_SECURE_NFC_ON + "_" + userId;
+    }
+
+    /**
+     * Loads the Secure NFC setting for the given user.
+     * @param userId The user ID for which to load the settings.
+     */
+    private void loadSecureNfcSettings(int userId) {
+        String secureNfcPreferenceKey = getSecureNfcPreferenceKeyForUser(userId);
+        // Get the user-specific preference.
+        // Fall back to the device's default setting if not found.
+        mIsSecureNfcEnabled = mPrefs.getBoolean(secureNfcPreferenceKey,
+                mDeviceConfigFacade.getDefaultSecureNfcState())
+                && mIsSecureNfcCapable;
+
+        Log.i(TAG, "Reloaded Secure NFC setting for user "
+                + userId + ". Enabled: " + mIsSecureNfcEnabled);
+
+        // Apply the newly loaded setting to the NFC controller
+        mDeviceHost.setNfcSecure(mIsSecureNfcEnabled);
+    }
+
     /** Returns true if NFC has user restriction set. */
     private boolean isNfcUserRestricted() {
         return mUserManager.getUserRestrictions().getBoolean(
@@ -1298,8 +1327,9 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         mIsEuiccCapable = mContext.getResources().getBoolean(R.bool.enable_euicc_support)
                 && NfcInjector.NfcProperties.isEuiccSupported();
         mForegroundUtils = mNfcInjector.getForegroundUtils();
-        mIsSecureNfcCapable = mDeviceConfigFacade.isSecureNfcCapable();
-        mIsSecureNfcEnabled = mPrefs.getBoolean(PREF_SECURE_NFC_ON,
+        mIsSecureNfcCapable = mIsHceCapable && mDeviceConfigFacade.isSecureNfcCapable();
+        String secureNfcPreferenceKey = getSecureNfcPreferenceKeyForUser(mUserId);
+        mIsSecureNfcEnabled = mPrefs.getBoolean(secureNfcPreferenceKey,
             mDeviceConfigFacade.getDefaultSecureNfcState())
             && mIsSecureNfcCapable;
         mDeviceHost.setNfcSecure(mIsSecureNfcEnabled);
@@ -2668,7 +2698,18 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                     return false;
                 }
                 Log.i(TAG, "setNfcSecure: " + enable);
-                mPrefsEditor.putBoolean(PREF_SECURE_NFC_ON, enable);
+                final int currentUserId = getUserId();
+                final String secureNfcPreferenceKey =
+                        getSecureNfcPreferenceKeyForUser(currentUserId);
+                mPrefsEditor.putBoolean(secureNfcPreferenceKey, enable);
+                Log.i(TAG, "currentUserId: " + currentUserId);
+                // If the change is made by the Primary User (SYSTEM), use the setting
+                // as new default.
+                // Check for System User ID
+                if (currentUserId == UserHandle.SYSTEM.getIdentifier()) {
+                    mDeviceConfigFacade.setDefaultSecureNfcState(enable);
+                }
+
                 mPrefsEditor.apply();
                 mIsSecureNfcEnabled = enable;
                 mBackupManager.dataChanged();
@@ -6186,6 +6227,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                 if (DBG) Log.d(TAG, action + "mReceiver.onReceive: UserId: " + userId);
                 if (mIsHceCapable) {
                     mCardEmulationManager.onUserSwitched(getUserId());
+                    loadSecureNfcSettings(userId);
                 }
 
                 mNfcInjector.onUserSwitched();
